64cacfbecdfe27bbdee86984ebc5b61d7c3bee17
Vansh Garg <156638411+vanshgarg110505@users.noreply.github.com>
2024-12-14 23:49:51 +0530

    Create all3DigitArmstrongNumber.java

---
+++ all3DigitArmstrongNumber.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class Main {
+    static boolean isArmstrong(int n){
+        int original = n;
+        int sum = 0;
+        while(n>0){
+            int rem = n % 10;
+            n = n / 10;
+            sum = sum + rem * rem * rem;
+        }
+        if(sum == original){
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        for(int i = 100 ; i <= 1000 ; i++){
+            if(isArmstrong(i)){
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
