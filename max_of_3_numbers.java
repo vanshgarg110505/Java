2c879c3d1c05924a3665c6d6b87d428ffbc3fe5c
Vansh Garg <156638411+vanshgarg110505@users.noreply.github.com>
2024-12-15 11:03:33 +0530

    Create max_of_3_numbers.java

---
+++ max_of_3_numbers.java
@@ -0,0 +1,9 @@
+class Main {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 5;
+        int c = 1;
+        int max = Math.max(c,Math.max(a,b));
+        System.out.println(max);
+    }
+}
