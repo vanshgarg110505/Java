96f29c45f90f354c21affab23425e7e88734510b
Vansh Garg <156638411+vanshgarg110505@users.noreply.github.com>
2024-12-18 18:10:28 +0530

    Create cyclic_sort.java

---
+++ cyclic_sort.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        int arr[] = {5,4,3,2,1};
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void sort(int[] arr){
+        int i = 0;
+        while(i < arr.length){
+            int correct = arr[i] - 1;
+            if(arr[i] != arr[correct]){
+                int temp = arr[i];
+                arr[i] = arr[correct];
+                arr[correct] = temp;
+            }
+            else{
+                i++;    
+            }
+        }
+    }
+}
