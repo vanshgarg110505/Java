2757bb05120802ef7f9038b692efb498799220fa
Vansh Garg <156638411+vanshgarg110505@users.noreply.github.com>
2024-12-19 18:00:43 +0530

    Create full_triangle_pattern.java

---
+++ Patterns/full_triangle_pattern.java
@@ -0,0 +1,20 @@
+class Main {
+    public static void main(String[] args) {
+        int n = 3;
+        for(int i = 1; i <= n; i++){
+            for(int space = 1 ; space <= n - i; space++){
+                System.out.print(" ");
+            }
+            for(int j = 1 ; j <= 2*i-1; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
+/*
+      *
+     ***
+    *****
+*/
