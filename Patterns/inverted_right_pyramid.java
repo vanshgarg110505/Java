4c6409630f5cdc4cd200825a6af5be40aea83750
Vansh Garg <156638411+vanshgarg110505@users.noreply.github.com>
2024-12-19 16:06:40 +0530

    Update inverted_right_pyramid.java

--- Patterns/inverted_right_pyramid.java
+++ Patterns/inverted_right_pyramid.java
@@ -1,6 +1,6 @@
 class Main {
     public static void main(String[] args) {
-        int n = 4;
+        int n = 3;
         for(int i = 1; i <= n; i++){
             for(int j = n - i + 1 ; j > 0; j--){
                 System.out.print("*"+ " ");
