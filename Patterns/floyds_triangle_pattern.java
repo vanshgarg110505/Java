32d2f063436c044b84540b7cea8d986af6e8fb9b
Vansh Garg <156638411+vanshgarg110505@users.noreply.github.com>
2024-12-19 21:01:07 +0530

    Update floyds_triangle_pattern.java

--- Patterns/floyds_triangle_pattern.java
+++ Patterns/floyds_triangle_pattern.java
@@ -4,7 +4,7 @@ class Main {
         int start = 1;
         for(int i = 1 ; i <= n ; i++){
             for(int j = 1 ; j <= i ; j++){
-                System.out.print(start);
+                System.out.print(start + " ");
                 start++;
             }
             System.out.println();
