8acc65d0a1f69622b1e6eaaaa491be40a098ae58
Vansh Garg <156638411+vanshgarg110505@users.noreply.github.com>
2024-12-19 16:00:25 +0530

    Create inverted_right_pyramid.java

---
+++ Patterns/inverted_right_pyramid.java
@@ -0,0 +1,11 @@
+class Main {
+    public static void main(String[] args) {
+        int n = 4;
+        for(int i = 1; i <= n; i++){
+            for(int j = n - i + 1 ; j > 0; j--){
+                System.out.print("*"+ " ");
+            }
+            System.out.println();
+        }
+    }
+}
