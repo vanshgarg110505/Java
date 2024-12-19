52febee13a39a4d587a1a25edad30be915246988
Vansh Garg <156638411+vanshgarg110505@users.noreply.github.com>
2024-12-19 21:00:30 +0530

    Create floyds_triangle_pattern.java

---
+++ Patterns/floyds_triangle_pattern.java
@@ -0,0 +1,21 @@
+class Main {
+    public static void main(String[] args) {
+        int n = 3;
+        int start = 1;
+        for(int i = 1 ; i <= n ; i++){
+            for(int j = 1 ; j <= i ; j++){
+                System.out.print(start);
+                start++;
+            }
+            System.out.println();
+        }
+    }
+}
+
+/*
+
+1
+23
+456
+
+*/
