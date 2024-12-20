157547202f3759135b2f05e61d62c8a097b9c829
Vansh Garg <156638411+vanshgarg110505@users.noreply.github.com>
2024-12-20 11:45:58 +0530

    Create hollow_rectangle_pattern.java

---
+++ Patterns/hollow_rectangle_pattern.java
@@ -0,0 +1,24 @@
+class Main {
+    public static void main(String[] args) {
+        int n = 3;
+        for(int i = 1 ; i <= n ; i++){
+            for(int j = 1 ; j <= n ; j++){
+                if(i==1 || j==1 || i == n || j == n){
+                    System.out.print("*");    
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
+/*
+
+***
+* *
+***
+
+*/
