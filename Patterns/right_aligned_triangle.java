963a9d88818a8f5df1dc44ecd05cde6404444f46
Vansh Garg <156638411+vanshgarg110505@users.noreply.github.com>
2024-12-20 15:49:00 +0530

    Create right_aligned_triangle.java

---
+++ Patterns/right_aligned_triangle.java
@@ -0,0 +1,23 @@
+class Main {
+    public static void main(String[] args) {
+        int n = 3;
+        for (int i = 1; i <= n; i++) {
+            for(int space = 1 ; space <= n-i ; space++){
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println(); 
+        }
+    }
+}
+
+
+/*
+
+  *
+ **
+***
+
+*/
