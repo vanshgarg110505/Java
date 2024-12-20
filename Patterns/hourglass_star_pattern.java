af26089c916665dc49443f1f62770521b24bfd7f
Vansh Garg <156638411+vanshgarg110505@users.noreply.github.com>
2024-12-20 16:08:01 +0530

    Create hourglass_star_pattern.java

---
+++ Patterns/hourglass_star_pattern.java
@@ -0,0 +1,34 @@
+class Main {
+    public static void main(String[] args) {
+        int n = 3;
+        for (int i = 1; i <= n; i++) {
+            for(int space = 1 ; space <= i - 1 ; space++){
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= n - i + 1; j++) {
+                System.out.print("* ");
+            }
+            System.out.println(); 
+        }
+        for(int i = n - 1 ; i <= n ; i++){
+            for(int space = 1 ; space <= n - i ; space++){
+                System.out.print(" ");
+            }
+            for(int j = 1 ; j<=i ;j++){
+                System.out.print("* ");
+            }
+            System.out.println(); 
+        }
+    }
+}
+
+
+/*
+
+* * *
+ * *
+  *
+ * *
+* * *
+
+*/
