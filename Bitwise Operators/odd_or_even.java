66b5c135db3d2c18e42f00da36451ce1d27c9cda
Vansh Garg <156638411+vanshgarg110505@users.noreply.github.com>
2024-12-29 16:03:45 +0530

    Create odd_or_even.java

---
+++ Bitwise Operators/odd_or_even.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        int n = 5;
+        if((n & 1)== 1){
+            System.out.println("Odd");
+        }
+        else{
+            System.out.println("Even");
+        }
+    }
+}
