c1d2ddb15673067a156a18fea9949ae925c3eaee
Vansh Garg <156638411+vanshgarg110505@users.noreply.github.com>
2024-12-29 19:05:01 +0530

    Create magic_number.java

---
+++ Bitwise Operators/magic_number.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        int n = 6;
+        int ans = 0;
+        int base = 5;
+        while(n>0){
+            int last = n & 1;
+            n = n >> 1;
+            ans += last * base;
+            base = base * 5;
+        }
+        System.out.print(ans);
+    }
+}
+
+/*
+          5^3 5^2 5^1    Magic No
+          
+    1  =  0   0   1         5
+    2  =  0   1   0         25
+    3  =  0   1   1         30
+
+*/
