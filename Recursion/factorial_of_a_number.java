8e02c45acb8c17c6c7fd2cb3b00fb61f9d2bca1e
Vansh Garg <156638411+vanshgarg110505@users.noreply.github.com>
2024-12-30 16:52:49 +0530

    Create factorial_of_a_number.java

---
+++ Recursion/factorial_of_a_number.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      int ans = func(5);
+      System.out.print(ans);
+    }
+    static int func(int n){
+      if(n==0){
+        return 1;
+      }
+      return n * func(n-1);
+    }
+} 
