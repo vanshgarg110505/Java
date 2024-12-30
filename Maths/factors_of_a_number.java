e22767986e19d24482423432bbb24f3754931a35
Vansh Garg <156638411+vanshgarg110505@users.noreply.github.com>
2024-12-30 12:26:12 +0530

    Create factors_of_a_number.java

---
+++ Maths/factors_of_a_number.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      int n = 20;
+      for(int i = 1 ; i<= n ; i++){
+        if(n%i==0){
+          System.out.print(i + " ");
+        }
+      }
+    }
+}
