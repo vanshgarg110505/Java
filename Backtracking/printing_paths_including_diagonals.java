580dc8b34a91297c34f672b01085dd9fe0486dcc
Vansh Garg <156638411+vanshgarg110505@users.noreply.github.com>
2024-12-31 23:21:38 +0530

    Update printing_paths_including_diagonals.java

--- Backtracking/printing_paths_including_diagonals.java
+++ Backtracking/printing_paths_including_diagonals.java
@@ -1,1 +1,27 @@
+import java.util.*;
 
+public class Main {
+    public static void main(String[] args) {
+      System.out.println(path("",3,3));        
+    }
+  
+    static ArrayList<String> path(String p, int r, int c) {
+        if(r == 1 && c == 1){
+          ArrayList<String> list = new ArrayList<>();
+          list.add(p);
+          return list;
+        }
+        
+        ArrayList<String> list = new ArrayList<>();
+        if(r>1){
+          list.addAll(path(p+'V', r-1, c));
+        }
+        if(c>1){
+          list.addAll(path(p+'H', r, c-1));
+        }
+        if(r>1 && c>1){
+          list.addAll(path(p+'D', r-1, c-1));
+        }
+        return list;
+    }
+}
