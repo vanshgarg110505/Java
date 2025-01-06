15301ec200d51331615bda8353a0bf397a87417b
Vansh Garg <156638411+vanshgarg110505@users.noreply.github.com>
2025-01-06 13:19:22 +0530

    Create 415_add_strings.java

---
+++ LeetCode/415_add_strings.java
@@ -0,0 +1,10 @@
+import java.math.BigInteger;
+
+class Solution {
+    public String addStrings(String num1, String num2) {
+        BigInteger n1 = new BigInteger(num1);
+        BigInteger n2 = new BigInteger(num2);
+        BigInteger ans = n1.add(n2);
+        return ans.toString();
+    }
+}
