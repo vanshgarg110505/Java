225a1ce53d45ccba85a0aef9a604cb324523573c
Vansh Garg <156638411+vanshgarg110505@users.noreply.github.com>
2025-01-07 22:57:57 +0530

    Create 162_find_peak_element.java

---
+++ LeetCode/162_find_peak_element.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int max = 0;
+        for(int i = 1 ; i < nums.length ; i++){
+            if(nums[i] > nums[max]){
+                max = i;
+            }
+        }
+        return max;
+    }
+}
