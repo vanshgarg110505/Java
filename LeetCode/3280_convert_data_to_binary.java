0b0fe76fd8d7f36709aa4615e6f30393420d60da
Vansh Garg <156638411+vanshgarg110505@users.noreply.github.com>
2025-01-09 19:51:40 +0530

    Create 3280_convert_data_to_binary.java

---
+++ LeetCode/3280_convert_data_to_binary.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String convertDateToBinary(String date) {
+        String[] parts = date.split("-");
+        int year = Integer.parseInt(parts[0]);
+        int month = Integer.parseInt(parts[1]);
+        int day = Integer.parseInt(parts[2]);
+
+        String binaryYear = Integer.toBinaryString(year);
+        String binaryMonth = Integer.toBinaryString(month);
+        String binaryDay = Integer.toBinaryString(day);
+
+        return String.format("%s-%s-%s", binaryYear, binaryMonth, binaryDay);
+
+    }
+}
