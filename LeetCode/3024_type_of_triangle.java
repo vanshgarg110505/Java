c6c61c188484b9c0893bcb75344c41f52db9961f
Vansh Garg <156638411+vanshgarg110505@users.noreply.github.com>
2025-01-09 19:24:51 +0530

    Update 3024_type_of_triangle.java

--- LeetCode/3024_type_of_triangle.java
+++ LeetCode/3024_type_of_triangle.java
@@ -1,7 +1,7 @@
 class Solution {
     public String triangleType(int[] nums) {
         Arrays.sort(nums);
-        if(nums[0] + nums[1] < nums[2]){
+        if(nums[0] + nums[1] <= nums[2]){
             return "none";
         }
         if(nums[0] == nums[1] && nums[1] == nums[2]){
