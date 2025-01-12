8eebbcdf0d468da43311412bc9e65722b6797bb2
Vansh Garg <156638411+vanshgarg110505@users.noreply.github.com>
2025-01-12 20:52:11 +0530

    Create 1071_greatest_common_divisors_of_strings.java

---
+++ LeetCode/1071_greatest_common_divisors_of_strings.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String gcdOfStrings(String str1, String str2) {
+        if(!(str1 + str2).equals(str2 + str1)){
+            return "";
+        }
+        int lengcd = gcd(str1.length(), str2.length());
+        return str1.substring(0, lengcd);
+    }
+
+    public int gcd(int a, int b){
+        if(a == 0){
+            return b;
+        }
+        return gcd(b%a, a);
+    }
+}
