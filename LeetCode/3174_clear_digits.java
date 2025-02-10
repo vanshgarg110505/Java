46be0194ddd60f03653392f94dc53be5f9deaf31
Vansh Garg <156638411+vanshgarg110505@users.noreply.github.com>
2025-02-10 14:29:18 +0530

    Create 3174_clear_digits.java

---
+++ LeetCode/3174_clear_digits.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String clearDigits(String s) {
+        StringBuilder sb = new StringBuilder();
+        for(char ch : s.toCharArray()){
+            if(Character.isDigit(ch)){
+                if(sb.length() > 0){
+                    sb.deleteCharAt(sb.length() - 1);
+                }
+            }
+            else{
+                sb.append(ch);
+            }
+        }
+        return sb.toString();
+    }
+}
