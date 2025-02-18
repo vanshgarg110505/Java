4ac1725cab68a7a58d34ef2c5bb914e7b82e8a6a
Vansh Garg <156638411+vanshgarg110505@users.noreply.github.com>
2025-02-18 09:58:48 +0530

    Create 1491_average_salary_excluding_the_minimum_and_maximum_salary.java

---
+++ LeetCode/1491_average_salary_excluding_the_minimum_and_maximum_salary.java
@@ -0,0 +1,11 @@
+class Solution {
+    public double average(int[] salary) {
+        Arrays.sort(salary);
+        double sum = 0;
+        for(int i = 1 ; i < salary.length - 1; i++){
+            sum += salary[i];
+        }
+        int n = salary.length - 2;
+        return sum / n;
+    }
+}
