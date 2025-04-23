0277a1bededeca155b243e1fd7d4f5f910e85195
Vansh Garg <156638411+vanshgarg110505@users.noreply.github.com>
2025-04-23 20:44:46 +0530

    Create 686_repeated_string_match.java

---
+++ LeetCode/686_repeated_string_match.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int repeatedStringMatch(String a, String b) {
+        StringBuilder sb = new StringBuilder();
+        int count = 0;
+
+        while(sb.length() < b.length()){
+            sb.append(a);
+            count++;
+        }
+
+        if(sb.toString().contains(b)){
+            return count;
+        }
+
+        sb.append(a);
+        if(sb.toString().contains(b)){
+            return count + 1;
+        }
+        return -1;
+    }
+}
