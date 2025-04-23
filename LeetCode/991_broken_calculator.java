63ae329705b2b7f0f4af16223532b0be01b0ba06
Vansh Garg <156638411+vanshgarg110505@users.noreply.github.com>
2025-04-23 23:36:13 +0530

    Create 991_broken_calculator.java

---
+++ LeetCode/991_broken_calculator.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int brokenCalc(int startValue, int target) {
+        int operations = 0;
+        while(target > startValue){
+            if(target % 2 == 0){
+                target /= 2;
+            }
+            else{
+                target++;
+            }
+            operations++;
+        }
+        return operations + (startValue - target);
+    }
+}
