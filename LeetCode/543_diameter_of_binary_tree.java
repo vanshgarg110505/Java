822f94f0cbf361829b5c1de3f684568b511c85e4
Vansh Garg <156638411+vanshgarg110505@users.noreply.github.com>
2025-05-14 23:39:05 +0530

    Create 543_diameter_of_binary_tree.java

---
+++ LeetCode/543_diameter_of_binary_tree.java
@@ -0,0 +1,19 @@
+class Solution {
+    int diameter = 0;
+    public int diameterOfBinaryTree(TreeNode root) {
+        depth(root);
+        return diameter;
+    }
+
+    public int depth(TreeNode node){
+        if(node == null){
+            return 0;
+        }
+
+        int left = depth(node.left);
+        int right = depth(node.right);
+
+        diameter = Math.max(diameter, left + right);
+        return 1 + Math.max(left, right);
+    }
+}
