696b5bd21ce1285db8e2643a7a0f4d7846c82975
Vansh Garg <156638411+vanshgarg110505@users.noreply.github.com>
2025-06-11 21:54:21 +0530

    Create 189_rotate_array.java

---
+++ LeetCode/189_rotate_array.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        k = k % nums.length;
+        int temp[] = new int[nums.length];
+        for(int i = 0 ; i < nums.length ; i++){
+            temp[(i+k) % nums.length] = nums[i];
+        }
+
+        for(int i = 0 ; i < nums.length ; i++){
+            nums[i] = temp[i];
+        }
+    }
+}
