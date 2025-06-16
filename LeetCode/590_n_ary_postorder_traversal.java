9ced0f67c46b674fa1dc7c927fe25f433dbd6eb5
Vansh Garg <156638411+vanshgarg110505@users.noreply.github.com>
2025-06-16 23:28:31 +0530

    Create 590_n_ary_postorder_traversal.java

---
+++ LeetCode/590_n_ary_postorder_traversal.java
@@ -0,0 +1,13 @@
+class Solution {
+    List<Integer> result = new ArrayList<>();
+    public List<Integer> postorder(Node root) {
+        if(root == null){
+            return result;
+        }
+        for(Node child : root.children){
+            postorder(child);
+        }
+        result.add(root.val);
+        return result;
+    }
+}
