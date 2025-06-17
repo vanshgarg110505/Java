d7823b6ea24e2b487e0bea8a53ddcc68c59ead9f
Vansh Garg <156638411+vanshgarg110505@users.noreply.github.com>
2025-06-17 20:20:12 +0530

    Create 78_subsets.java

---
+++ LeetCode/78_subsets.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(0, nums, new ArrayList<>(), result);
+        return result;
+    }
+
+    public void backtrack(int start, int[] nums, List<Integer> path, List<List<Integer>> result){
+        result.add(new ArrayList<>(path));
+        for(int i = start ; i < nums.length ; i++){
+            path.add(nums[i]);
+            backtrack(i + 1 , nums, path, result);
+            path.remove(path.size() - 1);
+        }
+    }
+}
