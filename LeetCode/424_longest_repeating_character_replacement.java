4a7402015aa133433fc28e909405aebcca13dfc5
Vansh Garg <156638411+vanshgarg110505@users.noreply.github.com>
2025-06-18 14:02:04 +0530

    Create 424_longest_repeating_character_replacement.java

---
+++ LeetCode/424_longest_repeating_character_replacement.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int count[] = new int[26];
+        int maxLength = 0, left = 0, maxCount = 0;
+        for(int right = 0 ; right < s.length(); right++){
+            count[s.charAt(right) - 'A']++;
+
+            maxCount = Math.max(maxCount, count[s.charAt(right) - 'A']);
+            if((right - left + 1) - maxCount > k){
+                count[s.charAt(left) - 'A']--;
+                left++;
+            }
+
+            maxLength = Math.max(maxLength, right - left + 1);
+        }
+        return maxLength;
+    }
+}
