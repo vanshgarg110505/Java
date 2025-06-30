2a1285f20f61631695f593e8d941e131853e7508
Vansh Garg <156638411+vanshgarg110505@users.noreply.github.com>
2025-06-30 22:21:43 +0530

    Create 942_DI_string_match.java

---
+++ LeetCode/942_DI_string_match.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] diStringMatch(String s) {
+        int n = s.length();
+        int result[] = new int[n + 1];
+        int low = 0 , high = n;
+        for(int i = 0 ; i < n ; i++){
+            if(s.charAt(i) == 'I'){
+                result[i] = low++;
+            }
+            else{
+                result[i] = high--;
+            }
+        }
+        result[n] = high;
+        return result;
+    }
+}
