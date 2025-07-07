69798eb61195086f5dce09f205cb8da06e5cec99
Vansh Garg <156638411+vanshgarg110505@users.noreply.github.com>
2025-07-07 22:40:19 +0530

    Create 279_perfect_squares.java

---
+++ LeetCode/279_perfect_squares.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int numSquares(int n) {
+        int dp[] = new int[n + 1];
+        for(int i = 1 ; i <= n ; i++){
+            dp[i] = i;
+        }
+
+        for(int i = 1 ; i <= n ; i++){
+            for(int j = 1 ; j * j <= i ; j++){
+                dp[i] = Math.min(dp[i], 1 + dp[i-j*j]);
+            }
+        }
+        return dp[n];
+    }
+}
