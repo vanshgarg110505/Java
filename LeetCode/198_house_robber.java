f254b9e9a7fde7fd0952a8d00fb69ee4ceb0e957
Vansh Garg <156638411+vanshgarg110505@users.noreply.github.com>
2025-07-11 10:45:39 +0530

    Create 198_house_robber.java

---
+++ LeetCode/198_house_robber.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int rob(int[] nums) {
+        if(nums.length == 0) return 0;
+        if(nums.length == 1) return nums[0];
+
+        int prev2 = 0;
+        int prev1 = nums[0];
+        for(int i = 1 ; i < nums.length ; i++){
+            int current = Math.max(prev1, nums[i] + prev2);
+            prev2 = prev1;
+            prev1 = current;
+        }
+        return prev1;
+    }
+}
